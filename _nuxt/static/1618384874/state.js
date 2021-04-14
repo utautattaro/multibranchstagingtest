6c2fe2da462c315204c229b59f1beb54614758d6
utautattaro <utautattaro@users.noreply.github.com>
2021-04-14 07:21:16 +0000

    Deploying to po/functiona from  @ d0c3123c4e62d2d97119ec0e8b1ee29cc7c9ee0d 🚀

---
+++ _nuxt/static/1618384874/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmultibranchstagingtest\u002F_nuxt\u002Fstatic\u002F1618384874",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmultibranchstagingtest\u002F",assetsPath:"\u002Fmultibranchstagingtest\u002F_nuxt\u002F",cdnURL:a}}}}(null));
